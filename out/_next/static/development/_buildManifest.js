8978d69c0351ed06141a91b24309a9bd9fbecba8
Mark <cmaynard@gmail.com>
2020-11-11 14:01:59 -0500

    Deploy Next.js to gh-pages

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/images":["static\u002Fchunks\u002Fpages\u002Fimages.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fimages"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
