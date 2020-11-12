f93cef59e2c1175930ae4eab02f38ae8b6600e29
Mark <cmaynard@gmail.com>
2020-11-12 18:01:28 -0500

    Added album

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@ export default function Home() {
   return (
     <div>
       Hello World.{' '}
+      <br/>
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
+      <br/>
       <Link href="/images" as={process.env.BACKEND_URL + '/images'}>
         <a>Images</a>
+      </Link>
+      <br/>
+      <Link href="/album" as={process.env.BACKEND_URL + '/album'}>
+        <a>Album</a>
       </Link>
     </div>
   )
